ca4b7a605939655369e2a3fa8dea14a210f8957e
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-16 08:41:10 -0500

    Added mouse aiming

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        // Player movement
         float dt = Time.deltaTime;
         Vector2 direction = Vector2.zero;
         if (Input.GetKey(KeyCode.W))
@@ -30,8 +31,16 @@ public class Player : MonoBehaviour
             direction += Vector2.right;
         }
         direction = direction.normalized;
-
         Vector3 movement = direction * moveSpeed * dt;
         transform.position += movement;
+
+        // Convert mouse from screen-space to world-space
+        Vector3 mouse = Input.mousePosition;
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+        mouse.z = 0.0f;
+
+        Vector3 mouseDirection = (mouse - transform.position).normalized;
+        Debug.DrawLine(transform.position, transform.position + mouseDirection * 5.0f);
+        
     }
 }
