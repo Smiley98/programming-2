f3123722814fd0029684d66da5f0fec943d9b87f
Smiley98 <smileysofficialcinema@gmail.com>
2025-02-13 12:36:26 -0500

    Switched to damage interface

--- Assets/Scripts/ArmoredTarget.cs
+++ Assets/Scripts/ArmoredTarget.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
-public class ArmoredTarget : MonoBehaviour
+public class ArmoredTarget : MonoBehaviour, IDamageable
 {
     float health = 100.0f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage * 0.5f;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         Projectile projectile = collision.GetComponent<Projectile>();
-        health -= projectile.damage * 0.5f;
+        TakeDamage(projectile.damage);
         if (health <= 0.0f)
             Destroy(gameObject);            // Destroy the target
         Destroy(collision.gameObject);      // Destroy the bullet
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
-public class Target : MonoBehaviour
+public class Target : MonoBehaviour, IDamageable
 {
     float health = 100.0f;
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         Projectile projectile = collision.GetComponent<Projectile>();
-        health -= projectile.damage;
+        TakeDamage(projectile.damage);
         if (health <= 0.0f)
             Destroy(gameObject);            // Destroy the target
         Destroy(collision.gameObject);      // Destroy the bullet
