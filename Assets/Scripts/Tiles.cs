2d5c56275444a16ad022b405a603112bc82c0b92
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-06 12:57:51 -0500

    Added gradient

--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -3,18 +3,29 @@ using UnityEngine;
 public class Tiles : MonoBehaviour
 {
     const int cols = 20;
+    const int rows = 10;
 
     [SerializeField]
     GameObject tilePrefab;
 
     void Start()
     {
-        float x = 0.5f;
-        for (int col = 0; col < cols; col++)
+        float y = 0.5f;
+        for (int row = 0; row < rows; row++)
         {
-            GameObject tile = Instantiate(tilePrefab);
-            tile.transform.position = new Vector3(x, 0.5f);
-            x += 1.0f;
+            float x = 0.5f;
+            for (int col = 0; col < cols; col++)
+            {
+                float u = col / (float)(cols - 1);
+                float v = row / (float)(rows - 1);
+                Color color = new Color(u, v, 0.0f);
+
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.position = new Vector3(x, y);
+                tile.GetComponent<SpriteRenderer>().color = color;
+                x += 1.0f;
+            }
+            y += 1.0f;
         }
     }
 
