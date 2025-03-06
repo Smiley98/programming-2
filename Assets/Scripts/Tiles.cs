799fbda7e54da9f5bf23f813b0881f2b25ca3f38
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-06 13:05:34 -0500

    Added gradient function

--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -7,6 +7,7 @@ public class Tiles : MonoBehaviour
 
     [SerializeField]
     GameObject tilePrefab;
+    GameObject[,] tiles = new GameObject[rows, cols];
 
     void Start()
     {
@@ -16,21 +17,37 @@ public class Tiles : MonoBehaviour
             float x = 0.5f;
             for (int col = 0; col < cols; col++)
             {
-                float u = col / (float)(cols - 1);
-                float v = row / (float)(rows - 1);
-                Color color = new Color(u, v, 0.0f);
-
                 GameObject tile = Instantiate(tilePrefab);
                 tile.transform.position = new Vector3(x, y);
-                tile.GetComponent<SpriteRenderer>().color = color;
                 x += 1.0f;
+
+                // Store our tile for later use!
+                tiles[row, col] = tile;
             }
             y += 1.0f;
         }
+
+        Gradient();
     }
 
     void Update()
     {
         
+    }
+
+    void Gradient()
+    {
+        for (int row = 0; row < rows; row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                float u = col / (float)(cols - 1);
+                float v = row / (float)(rows - 1);
+                Color color = new Color(u, v, 0.0f);
+
+                GameObject tile = tiles[row, col];
+                tile.GetComponent<SpriteRenderer>().color = color;
+            }
+        }
     }
 }
