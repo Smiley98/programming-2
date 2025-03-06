5432b1403846c9768c05ff7937b091eff8148d13
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-06 09:01:42 -0500

    Added rows

--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -3,21 +3,27 @@ using UnityEngine;
 public class Tiles : MonoBehaviour
 {
     const int cols = 20;
+    const int rows = 10;
 
     [SerializeField]
     GameObject tilePrefab;
 
     void Start()
     {
-        float x = 0.5f;
-        for (int i = 0; i < cols; i++)
+        float y = 0.5f;
+        for (int row = 0; row < rows; row++)
         {
-            GameObject tile = Instantiate(tilePrefab);
-            tile.transform.position = new Vector3(x, 0.5f);
-            float u = tile.transform.position.x / (float)cols;
-            Color color = new Color(u, 0.0f, 0.0f);
-            tile.GetComponent<SpriteRenderer>().color = color;
-            x += 1.0f;
+            float x = 0.5f;
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.position = new Vector3(x, y);
+                float u = tile.transform.position.x / (float)cols;
+                Color color = new Color(u, 0.0f, 0.0f);
+                tile.GetComponent<SpriteRenderer>().color = color;
+                x += 1.0f;
+            }
+            y += 1.0f;
         }
     }
 
