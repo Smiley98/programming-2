b6ad76aa4c144151c2c8784a44e79241aeab0ab3
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-13 09:14:27 -0400

    Added button name change

--- Assets/Scripts/UserInterfaceManager.cs
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -1,9 +1,13 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UserInterfaceManager : MonoBehaviour
 {
+    [SerializeField]
+    TMP_InputField inputField;
+
     [SerializeField]
     TMP_Text text;
 
@@ -12,12 +16,40 @@ public class UserInterfaceManager : MonoBehaviour
 
     void Start()
     {
+        inputField.onSubmit.AddListener(OnInputSubmit);
         button.onClick.AddListener(OnButtonClick);
+
+        text.gameObject.SetActive(false);
+        button.gameObject.SetActive(false);
     }
 
+    // Runs when button is clicked
     void OnButtonClick()
     {
-        Debug.Log("Button clicked!");
-        text.enabled = !text.enabled;
+        ToggleButtonText();
+    }
+
+    // Runs when ENTER is pressed
+    void OnInputSubmit(string input)
+    {
+        // Stores user text and shows/hides UI elements
+        text.text = input;
+        inputField.gameObject.SetActive(false);
+        text.gameObject.SetActive(true);
+        button.gameObject.SetActive(true);
+
+        // Made this into a function since it needs to be done once on-input submit,
+        // and every time we click our button (every time we change our text status)
+        ToggleButtonText();
+    }
+
+    void ToggleButtonText()
+    {
+        // Toggle text visible/invisible
+        text.gameObject.SetActive(!text.gameObject.activeSelf);
+
+        // Change button message depending on text status
+        string buttonText = text.gameObject.activeSelf ? "Hide Name" : "Show Name";
+        button.GetComponentInChildren<TMP_Text>().text = buttonText;
     }
 }
