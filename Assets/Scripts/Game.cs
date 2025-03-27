3304f6ce39d78dd323b373e5a6fcec4ff4762d1a
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-27 08:42:17 -0400

    Added animation

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,8 +8,7 @@ public class Game : MonoBehaviour
     [SerializeField]
     GameObject enemy;
 
-    [SerializeField]
-    float t;
+    float time = 0.0f;
 
     void Start()
     {
@@ -18,9 +17,14 @@ public class Game : MonoBehaviour
 
     void Update()
     {
+        float dt = Time.deltaTime;
+        time += dt;
+        if (time > 1.0f)
+            time = 0.0f;
+
         Vector3 A = waypoints[0].transform.position;
         Vector3 B = waypoints[1].transform.position;
-        Vector3 P = Vector3.Lerp(A, B, t);
+        Vector3 P = Vector3.Lerp(A, B, time);
         enemy.transform.position = P;
     }
 }
