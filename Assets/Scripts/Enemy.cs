8adc4a0e775c6368335a5c83bf2c2bd7fe6d7700
Smiley98 <smileysofficialcinema@gmail.com>
2025-04-03 09:18:16 -0400

    Added homework 6 start

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,6 +16,14 @@ public class Enemy : MonoBehaviour
 
     Rigidbody2D rb;
 
+    enum State
+    {
+        PATROL,
+        ATTACK
+    }
+
+    State state = State.PATROL;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -23,15 +31,60 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        float dt = Time.deltaTime;
-        rb.AddForce(Steering.FollowLine(gameObject, waypoints, ref curr, ref next, moveSpeed, ahead));
-        transform.up = Vector3.RotateTowards(transform.up, rb.velocity.normalized, turnSpeed * dt, 0.0f);
-        Debug.DrawLine(transform.position, transform.position + transform.up * 5.0f, Color.green);
+        UpdateState();
+        switch (state)
+        {
+            case State.PATROL:
+                Patrol();
+                break;
 
-        // AB = B - A
+            case State.ATTACK:
+                Attack();
+                break;
+        }
+        rb.AddForce(Avoid());
+        transform.up = Vector3.RotateTowards(transform.up, rb.velocity.normalized, turnSpeed * Time.deltaTime, 0.0f);
+        Debug.DrawLine(transform.position, transform.position + transform.up * 5.0f, Color.green);
+    }
+
+    void UpdateState()
+    {
         Vector3 toPlayer = (player.transform.position - transform.position).normalized;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, toPlayer, detectRadius);
         bool playerHit = hit && hit.collider.CompareTag("Player");
-        Debug.DrawLine(transform.position, transform.position + toPlayer * detectRadius, playerHit ? Color.red : Color.green);
+        state = playerHit ? State.ATTACK : State.PATROL;
+        //Debug.DrawLine(transform.position, transform.position + toPlayer * detectRadius, playerHit ? Color.red : Color.green);
+    }
+
+    void Patrol()
+    {
+        rb.AddForce(Steering.FollowLine(gameObject, waypoints, ref curr, ref next, moveSpeed, ahead));
+    }
+
+    void Attack()
+    {
+        // Homework 6:
+        // Equip the enemy with a weapon of your choice from Weapons.cs and fire it at the player!
+        rb.AddForce(Steering.Seek(gameObject, player.transform.position, moveSpeed));
+    }
+
+    Vector3 Avoid()
+    {
+        float distance = detectRadius * 0.5f;
+        Vector3 dirRight = Quaternion.Euler(0.0f, 0.0f, -20.0f) * transform.up;
+        Vector3 dirLeft = Quaternion.Euler(0.0f, 0.0f, 20.0f) * transform.up;
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, dirLeft, distance);
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, dirRight, distance);
+
+        Vector3 force = Vector3.zero;
+        if (hitLeft && hitLeft.collider.CompareTag("Obstacle"))
+        {
+            force += Steering.Seek(gameObject, transform.right * distance, moveSpeed);
+        }
+        else if (hitRight && hitRight.collider.CompareTag("Obstacle"))
+        {
+            force += Steering.Seek(gameObject, -transform.right * distance, moveSpeed);
+        }
+        return force;
     }
 }
