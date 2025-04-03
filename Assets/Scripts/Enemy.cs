75f88d8e8946cb53a21c7fc55067b15bca175396
Smiley98 <smileysofficialcinema@gmail.com>
2025-04-03 08:56:47 -0400

    Added line of sight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
+    GameObject player;
+
+    [SerializeField]
     GameObject[] waypoints;
     int curr = 0;
     int next = 1;
     float ahead = 2.0f;
     float moveSpeed = 5.0f;
     float turnSpeed = 250.0f * Mathf.Deg2Rad;
+    float detectRadius = 5.0f;
 
     Rigidbody2D rb;
 
@@ -23,5 +27,11 @@ public class Enemy : MonoBehaviour
         rb.AddForce(Steering.FollowLine(gameObject, waypoints, ref curr, ref next, moveSpeed, ahead));
         transform.up = Vector3.RotateTowards(transform.up, rb.velocity.normalized, turnSpeed * dt, 0.0f);
         Debug.DrawLine(transform.position, transform.position + transform.up * 5.0f, Color.green);
+
+        // AB = B - A
+        Vector3 toPlayer = (player.transform.position - transform.position).normalized;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, toPlayer, detectRadius);
+        bool playerHit = hit && hit.collider.CompareTag("Player");
+        Debug.DrawLine(transform.position, transform.position + toPlayer * detectRadius, playerHit ? Color.red : Color.green);
     }
 }
